dcdf7239ba791971a4fc45551478f93f89a7dfde
Alex Norman <xnor@cycling74.com>
2025-06-23 15:28:19 -0700

    add c74 apt repo and needed packages

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -2,6 +2,8 @@
 
 install -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/"
 install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/cycling74.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/apt-cycling74-pubkey.asc "${ROOTFS_DIR}/usr/share/keyrings/apt-cycling74-pubkey.asc"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.list"
 
