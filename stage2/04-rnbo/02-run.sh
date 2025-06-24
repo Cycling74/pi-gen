e26f3519db98bb4ff7b593763d027eab2fcebf7d
Alex Norman <xnor@cycling74.com>
2025-06-24 11:26:32 -0700

    more updates from README-rpi.md in rnbooscquery repo

--- stage2/04-rnbo/02-run.sh
+++ stage2/04-rnbo/02-run.sh
@@ -4,4 +4,7 @@ on_chroot <<- EOF
 	apt-mark hold rnbooscquery
 	debconf-set-selections <<< "jackd2 jackd/tweak_rt_limits boolean true"
 	dpkg-reconfigure -fnoninteractive jackd2
+	echo "GOVERNOR=\"performance\"" > /etc/default/cpufrequtils
+	echo "RemoveIPC=no" >> /etc/systemd/logind.conf
+	echo snd-dummy >> /etc/modules
 EOF
