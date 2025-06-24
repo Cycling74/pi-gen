cb6db2e85dcbe8cf7d3c1d5d2183a9c513a626ef
Alex Norman <xnor@cycling74.com>
2025-06-23 17:10:26 -0700

    add RNBO_VERSION to build script

--- build.sh
+++ build.sh
@@ -220,6 +220,8 @@ export TIMEZONE_DEFAULT="${TIMEZONE_DEFAULT:-Europe/London}"
 
 export GIT_HASH=${GIT_HASH:-"$(git rev-parse HEAD)"}
 
+export RNBO_VERSION="${RNBO_VERSION:-1.4.0}"
+
 export PUBKEY_SSH_FIRST_USER
 
 export CLEAN
